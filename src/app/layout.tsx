464da19117213cdd0980a5923ced58bf0d9fd4eb
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2024-05-03 15:23:39 -0300

    update lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={`${anton.variable} ${titilium.variable}`}>
         <Providers>
           <Header/>
